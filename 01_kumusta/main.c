98dec5ffd78a45bda9b6e0ed047cbf4e02e91676
radioation <greggallardo@gmail.com>
2025-12-07 16:29:43 -0600

    first project

---
+++ 01_kumusta/main.c
@@ -0,0 +1,17 @@
+
+#include <conio.h>
+#include <atari.h>
+
+void main() {
+  clrscr();
+  // position the cursor, output text
+  gotoxy(10,1);
+  cputs("Kumusta Diagdig!\r\n\r\n");
+
+    
+
+  // Loop forever
+  while (1) {
+  }
+}
+

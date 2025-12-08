0247fd833fc41b4c4624a92d6793ea3ffe37e631
radioation <greggallardo@gmail.com>
2025-12-07 21:30:05 -0600

    adding C/Assembly example

--- 02_c_asm/src/main.c
+++ 02_c_asm/src/main.c
@@ -1,18 +1,35 @@
 #include <conio.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 
 
-
-unsigned char __fastcall__ u_add8(unsigned char a, unsigned char b);
-
+extern unsigned char __fastcall__ u_add8(unsigned char a, unsigned char b);
+extern void __fastcall__ zero10( int8_t* msg );
 
 void main(void)
 {
+    unsigned char i;
+    unsigned char message[10];
+
     unsigned char a = 10;
     unsigned char b = 13;
-    unsigned char result = u_add8(a,b);
 
+    // Aseembly call with two char params
+    unsigned char result = u_add8(a,b);
     cprintf("%u + %u = %u\r\n", a,b,result);
+
+    // pointer call
+    memset( message, 1, 10 );
+    for( i=0; i < 10; ++i ) {
+        cprintf("%d", message[i] );
+    }
+    zero10( message );
+    for( i=0; i < 10; ++i ) {
+        cprintf("%d", message[i] );
+    }
+    cprintf("\r\n");
+
 
     while(1){
     }

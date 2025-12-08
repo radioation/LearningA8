78ea9445e2a6726f579b46b37017aea00aabc159
radioation <greggallardo@gmail.com>
2025-12-07 19:58:28 -0600

    trying out some conio functions

--- 01_kumusta/main.c
+++ 01_kumusta/main.c
@@ -1,17 +1,46 @@
 
 #include <conio.h>
 #include <atari.h>
+#include <string.h>
+#include <stdio.h>
+
 
 void main() {
-  clrscr();
-  // position the cursor, output text
-  gotoxy(10,1);
-  cputs("Kumusta Diagdig!\r\n\r\n");
+    char message[40]; 
+    unsigned char a;
+    clrscr();
+    // position the cursor
+    gotoxy(10,1);
 
-    
+    // 
+    cputs("Kumusta Diagdig!\r\n\r\n");
 
-  // Loop forever
-  while (1) {
-  }
+    cprintf("OS TYPE: %d\r\n", get_ostype() );
+  
+    a = get_tv(); 
+    sprintf( message, "TV: %d", a );
+    cprintf("%s\r\n", message );
+
+
+    bgcolor( 0 );
+    // textcolor( 15 ); no effect.
+
+    cputs("Enter name: ");
+    memset( message, 0, sizeof( message ) );
+    cgets( message, sizeof(message) );
+    cprintf("Hi %s. Press any key.\r\n", message ); 
+     
+    // Loop forever
+    while (1) {
+        if( kbhit() ) {
+            a = cgetc();
+            gotoxy(16, 10 );
+            cprintf("Key: %d  ", a );
+            if( a >= 48 && a <= 57 ) {
+                bgcolor(a -48 );
+            }
+
+        }
+    }
 }
 
